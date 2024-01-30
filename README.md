a7731ed7ee717dfc0740458ea7c7acc5054cb370
Pramoth K J <108267585+pramoth-kj@users.noreply.github.com>
2024-01-30 12:01:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# One Page Portfolio
+### This repository contains a one-page portfolio showcasing my skills and experience as a front-end developer.
+
+## Technologies Used
+The portfolio is built using HTML, CSS, and JavaScript.
+
+## Mockup Design
+<img
+  src="/mockup/mockup_desktop_view.jpg"
+  alt="Mockup Design"
+  title="Optional title"
+  style="display: inline-block; margin: 0 auto; max-width: 200px">
+  #### A link to the psd file can be found [here](https://github.com/itsmeralph09/portfolio/blob/main/mockup/mockup_desktop_view.psd)
+## Features
+The portfolio includes the following sections:
+
+- About Me: A brief introduction to who I am and what I do.
+- Skills: A list of my relevant skills and expertise.
+- Projects: A showcase of my previous projects with descriptions, screenshots, and links.
+- Contact: A section with my contact information.
+
+## Usage
+To view the portfolio, simply open the index.html file in your web browser. Alternatively, you can view it online by visiting https://itsmeralph09.github.io/portfolio
+
+## Contributions
+Contributions are not currently being accepted for this project, as it is intended to showcase my own work. However, I welcome any feedback or suggestions for improvement.
+
+<!-- ## License
+This project is licensed under the [insert license type] license. See the LICENSE file for details. -->
+
+<!-- Acknowledgements
+I would like to thank [insert names of anyone you want to acknowledge] for their support and feedback on this project. -->
---
+++ script.js
@@ -0,0 +1,70 @@
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  }
+
+  if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+[...navLink].forEach((nav) => {
+  nav.addEventListener("click", () => {
+    if (primaryNav.getAttribute("data-visible") === "true") {
+      primaryNav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+    }
+  });
+});
+var typed = new Typed(".typing", {
+  strings: [
+    "Student",
+    "Data Analyst",
+    "Cyber Security enthusiast",
+    "Poster Designer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+let navbar1 = document.getElementById("primary-nav");
+let viewportHeight = window.innerHeight;
+let navHeight = document.getElementById("primary-nav").offsetHeight;
+
+let navbarLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", (e) => {
+  scrollpos = window.scrollY;
+  navbarLinks.forEach((link) => {
+    let section = document.querySelector(link.hash);
+    if (
+      section.offsetTop <= scrollpos + 140 &&
+      section.offsetTop + section.offsetHeight > scrollpos + 130
+    ) {
+      link.classList.add("nav-link-active");
+    } else {
+      link.classList.remove("nav-link-active");
+    }
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const nav = document.querySelector("header");
+  if (window.pageYOffset > 0) {
+    nav.classList.add("add-shadow");
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("add-shadow");
+    nav.classList.add("sticky");
+  }
+});
